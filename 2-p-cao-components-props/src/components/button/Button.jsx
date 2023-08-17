3d79cb10c99531d1b42b8e910e9b25d0a9b9a4fc
Mindaugas Biliunas <mind.biliunas@gmail.com>
2023-08-17 21:30:17 +0300

    app.jsx, welcome.jsx fix to display result

---
+++ 2-p-cao-components-props/src/components/button/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Button.css"
+import PropTypes from "prop-types";
+
+
+
+const Button = () => (
+    <div className="btn-wrapper">
+        <button type="button" className="btn">My Button</button>
+    </div>
+)
+
+export default Button
