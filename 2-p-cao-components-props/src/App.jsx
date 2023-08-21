dddf8b934977b1c2c7aee7cd92461e5f278257cc
Mindaugas Biliunas <mind.biliunas@gmail.com>
2023-08-21 22:27:51 +0300

    Added Heros.jsx / to fix

--- 2-p-cao-components-props/src/App.jsx
+++ 2-p-cao-components-props/src/App.jsx
@@ -1,7 +1,9 @@
 import Button from './components/button/Button.jsx';
+import Hero from "./components/Hero.jsx"
 
 const App=()=>(
    <Button/ >
+   <Hero/ >
 );
 export default App
 
--- 2-p-cao-components-props/src/components/Hero.jsx
+++ 2-p-cao-components-props/src/components/Hero.jsx
@@ -1,19 +1,20 @@
-// import React from 'react';
+import PropTypes from "prop-types"
 
-// const Hero = ({ title, subtitle, color }) => {
-//   const heroStyle = {
-//     backgroundColor: color,
-//     padding: '20px',
-//     color: 'white',
-//     textAlign: 'center'
-//   };
 
-//   return (
-//     <div style={heroStyle}>
-//       <h1>{title}</h1>
-//       <h2>{subtitle}</h2>
-//     </div>
-//   );
-// };
+const Hero = ({ title, subtitle, color }) => {
+  const heroStyle = {
+    backgroundColor: color,
+    padding: '20px',
+    color: 'white',
+    textAlign: 'center'
+  };
 
-// export default Hero;
+  return (
+    <div style={heroStyle}>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+    </div>
+  );
+};
+
+export default Hero;
