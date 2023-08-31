586c4e48776b2b66d01ac53192b37a9941212836
Mindaugas Biliunas <mind.biliunas@gmail.com>
2023-08-31 21:55:02 +0300

    Create event added

--- 9-p-event-manager/src/components/NavigationBar.jsx
+++ 9-p-event-manager/src/components/NavigationBar.jsx
@@ -5,8 +5,7 @@ import "./NavigationBar.scss";
 const NavigationBar = () => {
   return (
     <header className="navigation-header">
-      <img src="https://media.istockphoto.com/id/1284069443/vector/dog-with-bone.jpg?s=612x612&w=0&k=20&c=cmOLlczwDU03W3dtM3eHUqSOzGR4p049WrtO05iwmw8=" alt="logo" className="navigation-logo" />
-      <h1>HAPPY PETS EVENTS</h1>
+      <img src="" alt="logo" className="navigation-logo" />
       <nav>
         {topNavigationItems.map((navItem) => (
           <Link
