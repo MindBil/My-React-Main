f79e8bbd5b25872329ae058a61ea4f662fe60f9d
Mindaugas Billiunas <mind.biliunas@gmail.com>
2023-09-08 15:34:28 +0300

    Initialized route / not working

---
+++ routing-test/src/App.jsx
@@ -0,0 +1,37 @@
+// // src/App.js
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// src/App.js
+
+// src/App.js
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar /> {/* Include the Navbar component for navigation */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ routing-test/src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* Your home page content */}
+    </div>
+  );
+}
+
+export default Home;
