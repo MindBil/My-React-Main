23f1566db9a81b4ca69b10a16da4a93131ee627b
Mindaugas <137099858+MindBil@users.noreply.github.com>
2023-10-16 23:11:27 +0300

    removed nomatch

--- 7-p-routing/src/components/NavigationBar.jsx
+++ 7-p-routing/src/components/NavigationBar.jsx
@@ -5,7 +5,6 @@ const NavigationBar = () => {
     <nav style={{ display: "flex", justifyContent: "center", gap: "1rem" }}>
       <Link to="/">Home</Link>
       <Link to="/contacts">Contacts</Link>
-      <Link to="/about-us">About us</Link>
     </nav>
   );
 };
